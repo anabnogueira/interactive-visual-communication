873823a342c6107fc8f9f924dcbb3fde6f7c8ef9
Ana Nogueira <ana.nogueira.mu@gmail.com>
2019-04-03 16:27:35 +0100

    Add fixes to lab

--- Labs/1/medianExp.m
+++ Labs/1/medianExp.m
@@ -6,14 +6,13 @@ imageN = imnoise(img, 'salt & pepper', 0.42);
 figure, imshow(imageN);
 
 % medfilt2 : 2-D median filtering
-% medfilt2(IMG) performs median filterning of the matrix A using
-% the default 3x3 neighbourhood
+%   medfilt2(IMG) performs median filterning of the matrix A using
+%   the default 3x3 neighbourhood
 imageF2 = medfilt2(imageN);
 figure, imshow(imageF2);
 
 % alternative:
 % this alters the filter size to 5x5 :
-%imageF2 = medfilt2(imageN, [5 5]); 
-
-%figure, imgshow(imageF2);
+imageF2 = medfilt2(imageN, [5 5]); 
+figure, imshow(imageF2);
 
--- Labs/1/noiseImageLenna.m
+++ Labs/1/noiseImageLenna.m
@@ -2,15 +2,32 @@ clear all, close all
 
 imgg = imread('images/lena512.bmp');
 
-figure, imshow(imgg)
+%figure, imshow(imgg)
 image = imnoise(imgg, 'salt & pepper', 0.05);
-figure, imshow(image);
+%figure, imshow(image);
 
-% method 1
-% K = imfilter(image, fspecial('gaussian', 11, 1));
-K1 = imfilter(image, fspecial('average', 3));
+% BELOW:
+% imfilter - applies neutral density filter over image
+%   fspecial - creates filter of type with specific parameters
 
-% method 2
-K2 = medfilt2(image, [3,3]);
+% -- method 1 --
+%       ('gaussian', HSIZE, SIGMA)
+%           HSIZE - filter size, either AxB matrix or scalar (default 3x3)
+%           SIGMA - positive deviation (default 0.5)
+K1 = imfilter(image, fspecial('gaussian', 11, 1));
 figure, imshow(K1)
+
+% -- method 2 --
+%       ('average', HSIZE)
+%           HSIZE - filter size, either AxB matrix or scalar (default 3x3)
+
+K2 = imfilter(image, fspecial('average', 3));
 figure, imshow(K2)
+
+% -- method 3 --
+% medfilt2(A, [M N])
+% medfilt2 - performs median filtering of the matrix A in two dimensions
+% Each output pixel contains the median value in the M-by-N neighbourhood
+K3 = medfilt2(image, [3,3]);
+figure, imshow(K3)
+
